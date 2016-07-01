5cf4dd8bd8373c1709ef6a0c5f36eee05d77c044
Valantis Kamayiannis <v.kamayiannis@orosimosoftware.gr>
2016-07-01 17:58:02 +0300

    added filter lambda

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,6 +2,7 @@ class Route < ActiveRecord::Base
   belongs_to :customer
   belongs_to :transportation
   validates_presence_of :customer
+  scope :filters_from_calendar, lambda { |params| filter_by_date(params) }
 
   def customer_name
     customer.try(:name)
@@ -18,4 +19,13 @@ class Route < ActiveRecord::Base
   def transportation_shortcut=(shortcut)
     self.transportation = Transportation.find_by_shortcut(shortcut) if shortcut.present?
   end
+
+  def filter_by_date(params)
+    if params.nil?
+      return "1=1"
+    else
+      return "route_date = ?", params[:route_date]
+    end
+  end
+
 end
